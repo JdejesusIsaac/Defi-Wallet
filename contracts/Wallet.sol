6f3c243168dbb945ba42d10113a84149c50655a3
Angelica Colon <juandejesusisaac@gmail.com>
2021-12-24 09:28:37 -0500

    up

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -34,4 +34,59 @@ contract Wallet is Compound {
         uint compAmount = compToken.balanceOf(address(this));
         compToken.transfer(recipient, compAmount);
     }
+    
+    function withdraw(
+    address cTokenAddress, 
+    uint underlyingAmount,
+    address recipient
+  ) 
+    onlyAdmin()
+    external  
+  {
+    require(
+      getUnderlyingBalance(cTokenAddress) >= underlyingAmount, 
+      'balance too low'
+    );
+    claimComp();
+    redeem(cTokenAddress, underlyingAmount);
+
+    address underlyingAddress = getUnderlyingAddress(cTokenAddress); 
+    IERC20(underlyingAddress).transfer(recipient, underlyingAmount);
+
+    address compAddress = getCompAddress(); 
+    IERC20 compToken = IERC20(compAddress);
+    uint compAmount = compToken.balanceOf(address(this));
+    compToken.transfer(recipient, compAmount);
+  }
+
+  function withdrawEth(
+    uint underlyingAmount,
+    address payable recipient
+  ) 
+    onlyAdmin()
+    external  
+  {
+    require(
+      getUnderlyingEthBalance() >= underlyingAmount, 
+      'balance too low'
+    );
+    claimComp();
+    redeemEth(underlyingAmount);
+
+    recipient.transfer(underlyingAmount);
+
+    address compAddress = getCompAddress(); 
+    IERC20 compToken = IERC20(compAddress);
+    uint compAmount = compToken.balanceOf(address(this));
+    compToken.transfer(recipient, compAmount);
+  }
+
+  receive() external payable {
+    supplyEth(msg.value);
+  }
+
+  modifier onlyAdmin() {
+    require(msg.sender == admin, 'only admin');
+    _;
+  }
 }
